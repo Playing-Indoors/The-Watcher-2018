35fa07d78b7a7ec66315accad57a04873322bda4
Caleb Kester <ckester@coachingactuaries.com>
2018-07-01 12:01:34 -0500

    removing purge for now

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,21 @@
 /* eslint-disable no-undef */
-const PurgecssPlugin = require('purgecss-webpack-plugin');
-const glob = require('glob-all');
-const path = require('path');
+// const PurgecssPlugin = require('purgecss-webpack-plugin');
+// const glob = require('glob-all');
+// const path = require('path');
 
 module.exports = {
-	configureWebpack: {
-		plugins: [
-			new PurgecssPlugin({
-				whitelistPatterns: [/(_)/, /(-enter)/, /(-leave)/],
-				paths: glob.sync([
-					path.join(__dirname, './public/index.html'),
-					path.join(__dirname, './src/**/*.vue'),
-					path.join(__dirname, './src/**/*.js')
-				])
-			})
-		]
-	},
+	// configureWebpack: {
+	// 	plugins: [
+	// 		new PurgecssPlugin({
+	// 			whitelistPatterns: [/(_)/, /(-enter)/, /(-leave)/],
+	// 			paths: glob.sync([
+	// 				path.join(__dirname, './public/index.html'),
+	// 				path.join(__dirname, './src/**/*.vue'),
+	// 				path.join(__dirname, './src/**/*.js')
+	// 			])
+	// 		})
+	// 	]
+	// },
 
 	pwa: {
 		name: 'The Watcher',
